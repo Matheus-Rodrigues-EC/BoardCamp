0a2864ddc2d70a6152b4c80fd2c3a4ca6408b4f0
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-10 19:23:42 -0300

    Update gameMiddleware.js

--- src/middleware/gameMiddleware.js
+++ src/middleware/gameMiddleware.js
@@ -5,19 +5,7 @@ export function validateGame(req, res, next){
 
     if(gameSchema.validate({Name: name, Stock: stockTotal, Price: pricePerDay}).error !== undefined){
         
-        if(gameSchema.validate({Name: name, Stock: stockTotal, Price: pricePerDay}).error.message === '"Stock" must be greater than or equal to 1'){
-            return res.sendStatus(400);
-        }
-
-        if(gameSchema.validate({Name: name, Stock: stockTotal, Price: pricePerDay}).error.message === '"Price" must be greater than or equal to 1'){
-            return res.sendStatus(400);
-        }
-
-        if(gameSchema.validate({Name: name, Stock: stockTotal, Price: pricePerDay}).error.message === '"Name" is not allowed to be empty'){
-            return res.sendStatus(400);
-        }
-        console.log(gameSchema.validate({Name: name, Stock: stockTotal, Price: pricePerDay}).error.message);
-        return res.status(422).send(gameSchema.validate({Name: name, Stock: stockTotal, Price: pricePerDay}).error.message);
+        return res.sendStatus(400);
     }else{
         next();
     }
