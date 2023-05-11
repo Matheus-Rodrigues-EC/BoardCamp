3711bbada91080dddc5d1bb049cce8c25eb12b29
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-05-10 21:06:16 -0300

    Update customerController.js

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -59,10 +59,10 @@ export async function putCustomer(req, res){
     try{
         if(!cpf){
             await db.query('UPDATE customers SET name = $1, phone = $2, birthday = $3 WHERE id = $4', [name, phone, birthday, id]);
-            return res.sendStatus(201);
+            return res.sendStatus(200);
         }else{
             await db.query('UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5', [name, phone, cpf, birthday, id]);
-            return res.sendStatus(201);
+            return res.sendStatus(200);
         }
     }catch(error){
         return res.status(500).send(error)
